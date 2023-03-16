6362532de2e4fde1742cf88e7a758d950129083b
Meriem Ben Ayad <meriem.benayad@gmail.com>
2023-03-16 12:46:40 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- * main - \"Programming is like building a multilingual puzzle\"
+ * main - \"Programming is like building a multilingual puzzle
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-        puts("Programming is like building a multilingual puzzle\n");
-        return (0);
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return (0);
 }
