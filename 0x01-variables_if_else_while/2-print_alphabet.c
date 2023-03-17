da0f74119bb0c506c9e250c3859d3166c7fedd9b
Meriem Ben Ayad <meriem.benayad@gmail.com>
2023-03-17 11:48:30 -0700

    Alphabet game

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Lowercase Alphabet
+ *
+ * Return: abcdefghijklmnopqrstuvwxyz 
+ */
+int main(void)
+{
+	char letter = 'a';
+
+	/* Alphabet in Lowercase */
+	while (letter <= 'z')
+	{
+		putchar(letter);
+		letter++;
+	}
+	putchar('\n');
+	return (0);
+}
