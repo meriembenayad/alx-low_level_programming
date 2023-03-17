a922286adf6fd06b12df5836b3a5915adfadd63f
Meriem Ben Ayad <meriem.benayad@gmail.com>
2023-03-17 13:39:21 -0700

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Numbers of base 10
+ *
+ * Description: By using putchar()
+ *
+ * Return: 0123456789
+ */
+int main(void)
+{
+	int number = 0;
+	int ascii_num = number + '0';
+
+	/* Numbers of base 10 */
+	while (ascii_num <= '9')
+	{
+		putchar(ascii_num);
+		ascii_num++;
+	}
+	printf("\n");
+	return (0);
+}
