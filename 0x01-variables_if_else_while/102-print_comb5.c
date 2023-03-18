aa42c58f227ee6f72b5eeab1fe4a90cbaee400dd
Meriem Ben Ayad <meriem.benayad@gmail.com>
2023-03-17 19:49:55 -0700

    Different combinations of two two digits

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - Combinaitions of two two digits
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int dig1, dig2;
+
+	for (dig1 = 0; dig1 < 99; dig1++)
+	{
+		for (dig2 = dig1 + 1; dig2 < 100; dig2++)
+		{
+			putchar(dig1 / 10 + '0');
+			putchar(dig1 % 10 + '0');
+			putchar(' ');
+			putchar(dig2 / 10 + '0');
+			putchar(dig2 % 10 + '0');
+			if (dig1 == 98 && dig2 == 99)
+				continue;
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
