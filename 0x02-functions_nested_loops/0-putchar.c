5982eacd40de3201dc2148fc9ba23103957210f3
Meriem Ben Ayad <meriem.benayad@gmail.com>
2023-03-21 08:52:14 -0700

    README, _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include "main.c"
+/**
+ * main - prints _putchar character.
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char put_var[] = "_putchar";
+	int i;
+
+	for (i = 0; i < 8; i++)
+		_putchar(put_var[i]);
+	_putchar('\n');
+
+	return 0;
+}
