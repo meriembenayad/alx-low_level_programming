92c349b8ee6ca063b4a19bb2be5df7c428e00e6e
Meriem Ben Ayad <meriem.benayad@gmail.com>
2023-03-21 11:56:56 -0700

    Print Alphabet in Lowercase

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * function: print_alphabet()
+ */
+void print_alphabet(void)
+{
+	char alpha = 'a';
+
+	while (alpha <= 'z')
+	{
+		_putchar(alpha);
+		alpha++;
+	}
+	_putchar('\n');
+}
