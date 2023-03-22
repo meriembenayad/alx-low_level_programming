62a4ddb1c3078e86f333ce75e251e206eb488ebe
Meriem Ben Ayad <meriem.benayad@gmail.com>
2023-03-21 17:47:09 -0700

    98 Battery Street

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -13,9 +13,8 @@ void print_to_98(int n)
 		for (i = n; i <= 98; i++)
 		{
 			printf("%d", i);
-			if (i == 98)
-				continue;
-			printf(", ");
+			if (i < 98)
+				printf(", ");
 		}
 	}
 	else
@@ -23,9 +22,8 @@ void print_to_98(int n)
 		for (i = n; i >= 98; i--)
 		{
 			printf("%d", i);
-			if (i == 98)
-				continue;
-			printf(", ");
+			if (i > 98)
+				printf(", ");
 		}
 	}
 }
