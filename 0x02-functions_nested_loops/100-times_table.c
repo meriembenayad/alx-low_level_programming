f2854844b278ce6c84e4b5550c8ebaf7bed5bfae
Meriem Ben Ayad <meriem.benayad@gmail.com>
2023-03-21 19:22:46 -0700

    n times table (update)

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -7,20 +7,20 @@
 void print_times_table(int n)
 {
 	int i, j, r;
-	
+
 	if (n >= 0 && n <= 15)
 	{	
 		for (i = 0; i <= n; i++)
 		{
 			_putchar('0');
-			
+
 			for (j = 1; j <= n; j++)
 			{
 				_putchar(',');
 				_putchar(' ');
-				
+
 				r = i * j;
-				
+
 				if (r <= 99)
 					_putchar(' ');
 				if (r <= 9)
@@ -29,11 +29,11 @@ void print_times_table(int n)
 				if (r >= 100)
 				{
 					_putchar((r / 100) + '0');
-					_putchar((r % 10) % 10 + '0');
+					_putchar((r / 10) % 10 + '0');
 				}
 				else if (r <= 99 && r >= 10)
 				{
-					_putchar((r % 10) + '0');
+					_putchar((r / 10) + '0');
 				}
 				_putchar((r % 10) + '0');
 			}
