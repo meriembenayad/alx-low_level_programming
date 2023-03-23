db0a5a6cc7c6b50ef3969dface87b38063dda497
Meriem Ben Ayad <meriem.benayad@gmail.com>
2023-03-23 09:44:30 -0700

    Prints square

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * print_square - prints square, followed by newline
+ * @size: the size of square that should be printed
+ * Return: void
+ */
+void print_square(int size)
+{
+	int i, j;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 0; i < size; i++)
+		{
+			for (j = 0; j < size; j++)
+			{
+				_putchar(35);
+			}
+			_putchar('\n');
+		}
+	}
+}
