ae0a4a318130bb5f3d2a570f8cfb5e8de158c1f0
Meriem Ben Ayad <meriem.benayad@gmail.com>
2023-03-23 11:21:41 -0700

    prints 10 times the numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -12,15 +12,9 @@ void more_numbers(void)
 	{
 		for (j = 0; j <= 14; j++)
 		{
-			if (j < 10)
-			{
-				_putchar('0' + j);
-			}
-			else
-			{
+			if (j >= 10)
 				_putchar('0' + j / 10);
-				_putchar('0' + j % 10);
-			}
+			_putchar('0' + j % 10);
 		}
 		_putchar('\n');
 	}
