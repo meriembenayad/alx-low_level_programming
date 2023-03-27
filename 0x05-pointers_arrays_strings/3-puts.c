805a27d3108cadf585c084a0e83bed14b14e4a44
Mirangel <meriem.benayad@gmail.com>
2023-03-27 23:03:00 +0000

    puts

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,13 @@
+/**
+ * _puts - function that prints a string.
+ * @str: string that should be printed
+ * Return: void
+ */
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(*str++);
+		_putchar('\n');
+	}
+}
