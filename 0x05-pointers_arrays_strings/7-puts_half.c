470b2f8b8b8730b1cb03a708ccfa6b9d6dc694a8
Mirangel <meriem.benayad@gmail.com>
2023-03-28 00:33:10 +0000

    puts hals

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * puts_half - function that prints half of a string.
+ * @str: string should be printed
+ * Return: 0
+ */
+void puts_half(char *str)
+{
+	int i = 0;
+
+	while (str[i])
+		i++;
+
+	i /= 2;
+
+	while (str[i])
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+}
