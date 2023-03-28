2ad98dd013bbe3a3c0d1a8228c56959c3cd95847
Mirangel <meriem.benayad@gmail.com>
2023-03-28 00:16:55 +0000

    puts half

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -8,12 +8,12 @@ void puts_half(char *str)
 {
 	int i;
 
-	while (str[i] != '\0')
-		i++;
-
 	for (i = 0; str[i] != '\0'; i++)
+		continue;
+	i++
+	for (i /= 2; str[i] != '\0'; i++)
 	{
-		_putchar(str[i - 1]);
+		_putchar(str[i]);
 	}
 	_putchar('\n');
 }
