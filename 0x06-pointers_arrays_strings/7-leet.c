10e138ca4486cd2913a8828ceddfcb585d95e505
Mirangel <meriem.benayad@gmail.com>
2023-03-30 15:58:12 +0000

    leet

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * leet - function that encodes a string into 1337.
+ * @str: string that will be encoded
+ * Return: encoded string.
+ */
+char *leet(char *str)
+{
+	int i, j;
+	char letters[] = "aAeEoOtTlL";
+	char numbers[] = "4433007711";
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		for (j = 0; letters[j] != '\0'; j++)
+		{
+			if (str[i] == letters[j])
+				str[i] = numbers[j];
+		}
+	}
+
+	return (str);
+}
