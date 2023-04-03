45a6b16c8660ce7d58e74a66f2ddb0ccbf201d26
Mirangel <meriem.benayad@gmail.com>
2023-04-03 23:02:01 +0000

    strchr

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -7,15 +7,13 @@
  */
 char *_strchr(char *s, char c)
 {
-	int i;
+	unsigned int i;
 
 	for (i = 0; s[i] >= '\0'; i++)
 	{
 		if (s[i] == c)
 		{
-			s + i;
+			retur (s + i);
 		}
 	}
-
-	return (s + i);
 }
