4953c2843ea41c2e3cc60402044a0949347fc2a3
Mirangel <meriem.benayad@gmail.com>
2023-04-11 21:40:59 +0000

    strdup

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,30 @@
+#include <string.h>
+/**
+ * *_strdup - Duplicate string to new pointer
+ * @str: pointer to string that will be duplicate
+ * Return: Duplicated string || NULL
+ */
+char *_strdup(char *str)
+{
+	int i;
+	int size = strlen(str);
+	char *new_str = malloc(size * sizeof(char));
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+
+	if (!new_str)
+	{
+		free(new_str);
+		return (NULL);
+	}
+
+	for (i = 0; i < size; i++)
+	{
+		new_str[i] = str[i];
+	}
+	new_str[i] = '\0';
+	return (new_str);
+}
