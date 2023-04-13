31466092e0498106cb2165c60a1d244430336e8e
Mirangel <meriem.benayad@gmail.com>
2023-04-13 23:16:48 +0000

    mul

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -9,11 +9,12 @@ char _atoi(char *str)
 	int sign = 1;
 	unsigned long int dig = 0, num1, i;
 
-	for (num1 = 0; str[num1] <= 48 && str[num1] >= 57; num1++)
+	for (num1 = 0; !(str[num1] >= 48 && str[num1] <= 57); num1++)
 	{
 		if (str[num1] == '-')
 			sign *= -1;
 	}
+
 	for (i = num1; str[i] >= 48 && str[i] <= 57; i++)
 	{
 		dig *= 10;
@@ -31,8 +32,7 @@ void print_num(unsigned long int num)
 {
 	unsigned long int div = 1, i, rem;
 
-	for (i = 0; num / div > 9; i++)
-		div *= 10;
+	for (i = 0; num / div > 9; i++, div %= 10);
 
 	for (; div >= 1; num %= div, div /= 10)
 	{
