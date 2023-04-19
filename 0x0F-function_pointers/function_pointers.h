d14970b3f2c0d465af1c38736e76027f635af653
Mirangel <meriem.benayad@gmail.com>
2023-04-19 23:45:07 +0000

    int_index

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -2,6 +2,7 @@
 #define FUNCTION_PINTERS_H
 
 #include <stdio.h>
+#include <stdlib.h>
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
