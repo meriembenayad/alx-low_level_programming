17b00ff389077fd0028995e67b56ec83ac02b458
Mirangel <meriem.benayad@gmail.com>
2023-04-20 14:41:17 +0000

    print_strings

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -21,11 +21,10 @@ void print_strings(const char *separator, const unsigned int n, ...)
 			printf("%s", str);
 		else
 			printf("(nil)");
-		
+
 		if (separator != NULL && i < n - 1)
 			printf("%s", separator);
 	}
 	printf("\n");
-
 	va_end(args);
 }
