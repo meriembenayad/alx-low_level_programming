d92858928f3d69e20b9ada4294def10df5519cd6
Mirangel <meriem.benayad@gmail.com>
2023-04-20 14:26:02 +0000

    print_strings & print_all

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -5,7 +5,7 @@
 void print_all(const char *const format, ...)
 {
 	va_list args;
-	int i = 0;
+	unsigned int i = 0;
 	char *str;
 	char *separator;
 
@@ -38,5 +38,6 @@ void print_all(const char *const format, ...)
 		i++;
 	}
 	va_end(args);
+
 	printf("\n");
 }
