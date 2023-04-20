b8b0397955b5ad8cfc53cabd1309809521595244
Mirangel <meriem.benayad@gmail.com>
2023-04-20 14:19:08 +0000

    print_strings

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -24,7 +24,6 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		if (i < n - 1 && separator != NULL)
 			printf("%s", separator);
 	}
-
-	va_end(args);
 	printf("\n");
+	va_end(args);
 }
