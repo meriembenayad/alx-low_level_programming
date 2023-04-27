e8341400bacd7536cb3884dfbcbe4c220c0ebb59
meriembenayad <meriem.benayad@gmail.com>
2023-04-27 20:15:55 +0000

    print_list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -13,7 +13,7 @@ size_t print_list(const list_t *h)
 		if (h->str)
 			printf("[%u] %s\n", h->len, h->str);
 		else
-			printf("[0] (nil)");
+			printf("[0] (nil)\n");
 		h = h->next;
 		count++;
 	}
