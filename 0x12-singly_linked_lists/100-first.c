fd51f57225db9a49d937f8ae811dffb9a8f6ef34
meriembenayad <meriem.benayad@gmail.com>
2023-04-27 23:47:28 +0000

    betty

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,11 +1,14 @@
 #include "lists.h"
-
 void print_before(void) __attribute__((constructor));
 /**
+ * print_before - function that prints string before main excute
  *
+ * Return: void
  */
 void print_before(void)
 {
-	char *str = "You're beat! and yet, you must allow,\nI bore my house upon my back!\n";
-	printf("%s", str);
+	char *str_1 = "You're beat! and yet, you must allow,\n";
+	char *str_2 = "I bore my house upon my back!\n";
+
+	printf("%s%s", str_1, str_2);
 }
