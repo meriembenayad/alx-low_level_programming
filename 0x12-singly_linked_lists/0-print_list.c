3c99293a71ad003aad13c15cf3f1c4ccc7796209
meriembenayad <meriem.benayad@gmail.com>
2023-04-27 20:06:33 +0000

    print list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ * print_list - prints all the elements of a list_t list
+ * @h: pointer to the head of the list
+ * Return: the number of nodes
+ */
+size_t print_list(const list_t *h)
+{
+	int count;
+
+	while (h)
+	{
+		if (h->str)
+			printf("[%u] %s\n", h->len, h->str);
+		else
+			printf("[0] (nil)");
+		h = h->next;
+		count++;
+	}
+}
---
+++ 0x12-singly_linked_lists/README.md
@@ -0,0 +1,1 @@
+##C - Singls Linked Lists
