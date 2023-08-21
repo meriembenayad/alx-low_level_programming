047b5823ebf3202e925cb232b1be0263a385e64b
meriembenayad <meriem.benayad@gmail.com>
2023-08-22 00:37:12 +0100

    Task 2 - Advanced

--- 0x18-dynamic_libraries/operations.c
+++ 0x18-dynamic_libraries/operations.c
@@ -1,60 +1,66 @@
 #include <stdio.h>
 /**
- * add_oper - Add two number
+ * add - Add two number
  * @num1: first integer
  * @num2: second integer
  *
  * Return: Addition of NUM1 + NUM2
  */
-int add_oper(int num1, int num2)
+int add(int num1, int num2)
 {
 	return (num1 + num2);
 }
 
 /**
- * sub_oper - Substruct two number
+ * sub - Substruct two number
  * @num1: first integer
  * @num2: second integer
  *
  * Return: Substruction of NUM1 - NUM2
  */
-int sub_oper(int num1, int num2)
+int sub(int num1, int num2)
 {
 	return (num1 - num2);
 }
 
 /**
- * mul_oper - Multiply two number
+ * mul - Multiply two number
  * @num1: first integer
  * @num2: second integer
  *
  * Return: Multiplication of NUM1 * NUM2
  */
-int mul_oper(int num1, int num2)
+int mul(int num1, int num2)
 {
 	return (num1 * num2);
 }
 
 /**
- * div_oper - Divise two number
+ * div - Divise two number
  * @num1: first integer
  * @num2: second integer
  *
  * Return: Division of NUM1 / NUM2
  */
-int div_oper(int num1, int num2)
+int div(int num1, int num2)
 {
-	return (num1 / num2);
+	if (num2 != 0)
+		return (num1 / num2);
+	else
+		return (0);
 }
 
 /**
- * mod_oper - Calculate reminder of divison of two number
+ * mod - Calculate reminder of divison of two number
  * @num1: first integer
  * @num2: second integer
  *
  * Return: Modulo of NUM1 % NUM2
  */
-int mod_oper(int num1, int num2)
+int mod(int num1, int num2)
 {
-	return (num1 % num2);
+	if (num2 != 0)
+		return (num1 % num2);
+	else
+		return (0);
 }
