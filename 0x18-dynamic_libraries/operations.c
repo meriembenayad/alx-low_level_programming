0ee654c4a2089898751129b19dc22cedbd70c970
meriembenayad <meriem.benayad@gmail.com>
2023-08-22 00:23:03 +0100

    Task 2 - Advanced

---
+++ 0x18-dynamic_libraries/operations.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+/**
+ * add_oper - Add two number
+ * @num1: first integer
+ * @num2: second integer
+ *
+ * Return: Addition of NUM1 + NUM2
+ */
+int add_oper(int num1, int num2)
+{
+	return (num1 + num2);
+}
+
+/**
+ * sub_oper - Substruct two number
+ * @num1: first integer
+ * @num2: second integer
+ *
+ * Return: Substruction of NUM1 - NUM2
+ */
+int sub_oper(int num1, int num2)
+{
+	return (num1 - num2);
+}
+
+/**
+ * mul_oper - Multiply two number
+ * @num1: first integer
+ * @num2: second integer
+ *
+ * Return: Multiplication of NUM1 * NUM2
+ */
+int mul_oper(int num1, int num2)
+{
+	return (num1 * num2);
+}
+
+/**
+ * div_oper - Divise two number
+ * @num1: first integer
+ * @num2: second integer
+ *
+ * Return: Division of NUM1 / NUM2
+ */
+int div_oper(int num1, int num2)
+{
+	return (num1 / num2);
+}
+
+/**
+ * mod_oper - Calculate reminder of divison of two number
+ * @num1: first integer
+ * @num2: second integer
+ *
+ * Return: Modulo of NUM1 % NUM2
+ */
+int mod_oper(int num1, int num2)
+{
+	return (num1 % num2);
+}
---
+++ 0x18-dynamic_libraries/operations.h
@@ -0,0 +1,10 @@
+#ifndef OPERATIONS_H
+#define OPERATIONS_H
+
+int add_oper(int num1, int num2);
+int sub_oper(int num1, int num2);
+int mul_oper(int num1, int num2);
+int div_oper(int num1, int num2);
+int mod_oper(int num1, int num2)
+
+#endif
