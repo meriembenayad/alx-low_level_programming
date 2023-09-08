b6470c002d2ea82513c64eb59b96d6f995e3991c
meriembenayad <meriem.benayad@gmail.com>
2023-09-08 17:37:08 +0100

    Tasks 0 - 3

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,43 @@
+#include "hash_tables.h"
+/**
+ * hash_table_set - Functon that adds an element to hash table
+ *
+ * @ht: hash table
+ * @key: the key of an element in hash table
+ * @value: the value of the key
+ *
+ * Return: 1 (SUCCESS) || 0 (FAILED)
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned long int index;
+	hash_node_t *new_node, *temp;
+
+	if (!ht || !key || !value)
+		return (0);
+
+	index = key_index((const unsigned char *)key, ht->size);
+	temp = ht->array[index];
+
+	while (temp)
+	{
+		if (strcmp(temp->key, key) == 0)
+		{
+			free(temp->value);
+			temp->value = strdup(value);
+			return (1);
+		}
+		temp = temp->next;
+	}
+
+	new_node = malloc(sizeof(hash_node_t));
+	if (!new_node)
+		return (0);
+
+	new_node->key = strdup(key);
+	new_node->value = strdup(value);
+	new_node->next = ht->array[index];
+	ht->array[index] = new_node;
+
+	return (1);
+}
