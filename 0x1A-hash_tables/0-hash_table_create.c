9f245b3b5d4e7f0735c6c795416af5148e540b24
meriembenayad <meriem.benayad@gmail.com>
2023-09-08 17:43:37 +0100

    Fix task 0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,7 +9,7 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *new_table;
-	unsigned long int i = 0;
+	unsigned long int i;
 
 	new_table = malloc(size * sizeof(hash_table_t));
 	if (new_table == NULL)
