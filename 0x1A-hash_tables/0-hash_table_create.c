34e609506a1bfecf6fe5a18cb02d7b84578ae8f8
meriembenayad <meriem.benayad@gmail.com>
2023-09-08 17:38:52 +0100

    Fix task 0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -18,7 +18,10 @@ hash_table_t *hash_table_create(unsigned long int size)
 	new_table->size = size;
 	new_table->array = malloc(sizeof(hash_node_t *) * size);
 	if (new_table->array == NULL)
-		return NULL;
+	{
+		free(new_table);
+		return (NULL);
+	}
 
 	for (i = 0; i < size; i++)
 		new_table->array[i] = NULL;
