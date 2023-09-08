bfa0f46c59acb63e31db749bbbf2403b6bb7d17f
meriembenayad <meriem.benayad@gmail.com>
2023-09-08 17:41:18 +0100

    Fix task 0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -16,7 +16,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 		return (NULL);
 
 	new_table->size = size;
-	new_table->array = calloc(sizeof(hash_node_t *) * size);
+	new_table->array = calloc(new_table->size, (hash_node_t *));
 	if (new_table->array == NULL)
 	{
 		free(new_table);
@@ -24,7 +24,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	}
 
 	for (i = 0; i < size; i++)
-		new_table->array[i++] = NULL;
+		new_table->array[i] = NULL;
 
 	return (new_table);
 }
