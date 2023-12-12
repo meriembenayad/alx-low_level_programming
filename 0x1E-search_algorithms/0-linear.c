9afbd353d1c6c7b1b5c454f4bd2ec37721ad7f7a
meriembenayad <meriem.benayad@gmail.com>
2023-12-12 13:14:23 +0100

    Task 0: Linear Search

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -13,14 +13,14 @@ int linear_search(int *array, size_t size, int value)
 	size_t i = 0;
 
 	if (!array)
-		return -1;
+		return (-1);
 
 	for (i = 0; i < size; i++)
 	{
 		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
 		if (array[i] == value)
-			return i;
+			return (i);
 	}
 
-	return -1;
+	return (-1);
 }
